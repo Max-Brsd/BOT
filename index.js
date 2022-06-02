c32699e142a882344d0f7bcabf45f17a7a9285c6
Maxime <maxime.brossard5@gmail.com>
2022-06-02 20:15:44 +0200

    Edit css

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
 
 client.once('ready',() =>{
     console.log('Le BOT à démarré');
-})
+});
+
+client.on('interactionCreate', async interaction =>{
+    if(!interaction.isCommand()){
+        return;
+    }
+
+    const { commandName } = interaction;
+
+    if(commandName === 'test'){
+        await interaction.reply('Salut toi');
+    }
+});
 
 client.login(config.token);
